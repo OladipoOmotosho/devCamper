0aa42da3ea51677f7c94d2ae60cb78e4174650f6
OladipoOmotosho <ishaq.omotosho@mail.concordia.ca>
2026-05-20 01:21:28 -0400

    routes folder and config created

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+//Route Files
+const bootcamps = require("./routes/bootcamps");
+
 //Load env variables
 dotenv.config({ path: "./config/config.env", debug: false });
 
 const app = express();
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
