36b5a40b39e798bee2788632fef88c8ce8363f87
OladipoOmotosho <ishaq.omotosho@mail.concordia.ca>
2026-05-30 23:20:13 -0400

    added a bunch of setup including setting up mongoDb

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
+const { getEnv } = require("./config");
 const express = require("express");
-const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
+
+//Connect to database
+
+connectDB();
 
 //Route Files
 const bootcamps = require("./routes/bootcamps");
-const logger = require("./middleware/logger");
-
-//Load env variables
-dotenv.config({ path: "./config/config.env", debug: false });
+// const logger = require("./middleware/logger");
 
 const app = express();
 
-//Mount routers
+//Dev logging middleware
+// app.use(logger);
 
-app.use(logger);
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
+}
+//Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT = process.env.PORT || 5000;
+const PORT = getEnv("PORT", 5000);
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+  console.log(
+    `Server running in ${getEnv("NODE_ENV", "development")} mode on port ${PORT}`
+      .yellow.bold,
+  ),
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+
+  // Close server and exit process
+
+  server.close(() => process.exit(1));
+});
