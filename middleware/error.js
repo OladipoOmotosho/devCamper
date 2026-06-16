a34cf761c16dfbe636555b4e8e54201d16444c70
OladipoOmotosho <ishaq.omotosho@mail.concordia.ca>
2026-06-15 21:44:37 -0400

    wrote custom error handler middleware

--- middleware/error.js
+++ middleware/error.js
@@ -6,13 +6,32 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   //log error to console for dev
-  console.log(err.stack.red);
+  console.log(err);
+
+  // Mongoose Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
 
   //Mongoos bad ObjectId
-
   if (err.name === "CastError") {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorResponse(message, 400);
+
+    // ASSUMING I WANT TO EXTRACT ALL INFORMATION
+    // const message = Object.values(err.errors).map((value) => ({
+    //   path: value.path,
+    //   message: value.message,
+    //   kind: value.kind,
+    //   reason: value.reason || "N/A",
+    // }));
   }
 
   res.status(error.statusCode || 500).json({
